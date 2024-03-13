c7948c6ccf6571c3bc2e6a9becb662b9b696fbcb
Travis CI User <travis@example.org>
2024-03-13 06:56:59 +0000

    docs: latest javadoc for 0.0.6 (b52b34ac82824e5b4daec6407c3bc6c57c31db55)

---
+++ docs/0.0.6/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.ibm.cloud.dpx_services.common"},{"l":"com.ibm.cloud.dpx_services.dpx.v1"},{"l":"com.ibm.cloud.dpx_services.dpx.v1.model"}]
